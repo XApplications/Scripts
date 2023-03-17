e971d64862c234e8ffe4e625c8a3cdce053b34be
chippytech <80016011+chippytech@users.noreply.github.com>
2023-03-17 13:47:39 -0500

    Update VistaScripts.js

--- VistaScripts.js
+++ VistaScripts.js
@@ -1,3 +1,14 @@
 function SetVpLogoImage(imageUrl) {
   document.getElementById("imgLogo").src = imageUrl;
 }
+function SetElementDetails(k, u, d, a) {
+	for (var i=0; i < a.length; i++) {
+		for (var x=0; x < a[i].items.length; x++) {
+			if (a[i].items[x].feature === k) {
+				if (d === 'url') a[i].items[x].url = u;
+				if (d === 'itemdesc') a[i].items[x].itemdesc = u;
+				if (d === 'searchtext') a[i].items[x].searchtext = u;
+			}
+		}
+	}
+}
